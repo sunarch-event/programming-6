9bcf798637061725340355b8be2dd89aab88b71b
procon-3-user6 <programming_3rd_contest_6@sunarch.co.jp>
2021-10-30 00:47:50 +0000

    20211030_lombokDataアノテーション導入

--- src/main/java/com/performance/domain/service/GoogleOauthResponse.java
+++ src/main/java/com/performance/domain/service/GoogleOauthResponse.java
@@ -2,6 +2,9 @@ package com.performance.domain.service;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Data;
+
+@Data
 public class GoogleOauthResponse {
 
     @JsonProperty("access_token")
@@ -11,28 +14,5 @@ public class GoogleOauthResponse {
     private String scope;
     @JsonProperty("token_type")
     private String tokenType;
-    public String getAccessToken() {
-        return accessToken;
-    }
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-    public int getExpiresIn() {
-        return expiresIn;
-    }
-    public void setExpiresIn(int expiresIn) {
-        this.expiresIn = expiresIn;
-    }
-    public String getScope() {
-        return scope;
-    }
-    public void setScope(String scope) {
-        this.scope = scope;
-    }
-    public String getTokenType() {
-        return tokenType;
-    }
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
+    
 }
